6d4b813fb701b59c60880d6df644a3f0f8083c00
Ayaad72 <ayaadalavi@gmail.com>
2025-01-11 06:43:49 -0800

    pushing changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/" element={<Home/>}
+            <Route path="/" element={<Homeee/>}
             <Route path="/search" element={<Filter />} />
           </Routes>
         </Layout>
