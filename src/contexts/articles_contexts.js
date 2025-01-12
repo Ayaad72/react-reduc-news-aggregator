ab988a2b121d04ff9a9645fbcf0f4a322e769cc8
Ayaad72 <ayaadalavi@gmail.com>
2025-01-12 10:25:11 -0800

    pushing changes

--- src/contexts/articles_contexts.js
+++ src/contexts/articles_contexts.js
@@ -6,7 +6,7 @@ const ArticlesContext = React.createContext({
   filterButtonValue: "",
   setFilterButtonValue: () => {},
   apiName: "",
-  setApiName: () => {},
+  asdsad: () => {},
 });
 
 export default ArticlesContext;
