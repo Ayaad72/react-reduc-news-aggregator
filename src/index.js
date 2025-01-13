c49e65c356039925c355527b94b3e41d6b4bb2eb
Ayaad72 <ayaadalavi@gmail.com>
2025-01-13 01:16:30 -0800

    pushing changes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ root.render(
   // </React.StrictMode>
 );
 
-// If you want to yess yess yess yess yess yess, pass a function
+// If you want to start measuring fsdfsdfsdf in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
